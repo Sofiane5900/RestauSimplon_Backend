4f4b070ab22fc11a761edf45573a8a25249307cc
Astambar <leuchartjason@gmail.com>
2025-01-08 13:49:48 +0100

    Add Categorie First Version

--- RestauSimplon/Categories.cs
+++ RestauSimplon/Categories.cs
@@ -1,8 +1,24 @@
-﻿namespace RestauSimplon
+﻿using Swashbuckle.AspNetCore.Annotations;
+namespace RestauSimplon
 {
-    public class Categories
+    public class Categorie
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
+        public int Id { get; set; }
+        public int? IdArticles {  get; set; }
+        public string Nom { get; set; } = null!;
+
+    }
+    public class CategorieDTO
+    {
+        [SwaggerSchema("Nom de la catégorie")]
+        public string Nom { get; set; } = null!;
+        [SwaggerSchema("Id de l'article")]
+        public int? IdArticles { get; set; }
+
+        public CategorieDTO(Categorie categorie)
+        {
+            this.Nom = categorie.Nom;
+            this.IdArticles= categorie.IdArticles;
+        }
     }
 }
