1b2af7db77a6f24f5ffb2658461c6222d71eb3f5
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-08 16:39:08 +0100

    ajout fichiers

---
+++ RestauSimplon/Migrations/20250108151808_InitialCreate.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestauSimplon.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorie",
+                columns: table => new
+                {
+                    Id = table
+                        .Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorie", x => x.Id);
+                }
+            );
+
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table
+                        .Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                    Prenom = table.Column<string>(type: "TEXT", nullable: false),
+                    Adresse = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<int>(type: "INTEGER", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                }
+            );
+
+            migrationBuilder.CreateTable(
+                name: "Commande",
+                columns: table => new
+                {
+                    Id = table
+                        .Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PrixTotal = table.Column<int>(type: "INTEGER", nullable: false),
+                    ClientId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ArticleId = table.Column<int>(type: "INTEGER", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commande", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Commande_Client_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade
+                    );
+                }
+            );
+
+            migrationBuilder.CreateTable(
+                name: "Article",
+                columns: table => new
+                {
+                    Id = table
+                        .Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                    Prix = table.Column<int>(type: "INTEGER", nullable: false),
+                    CategorieId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CommandeId = table.Column<int>(type: "INTEGER", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Article", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Article_Categorie_CategorieId",
+                        column: x => x.CategorieId,
+                        principalTable: "Categorie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade
+                    );
+                    table.ForeignKey(
+                        name: "FK_Article_Commande_CommandeId",
+                        column: x => x.CommandeId,
+                        principalTable: "Commande",
+                        principalColumn: "Id"
+                    );
+                }
+            );
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Article_CategorieId",
+                table: "Article",
+                column: "CategorieId"
+            );
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Article_CommandeId",
+                table: "Article",
+                column: "CommandeId"
+            );
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commande_ArticleId",
+                table: "Commande",
+                column: "ArticleId"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Article");
+
+            migrationBuilder.DropTable(name: "Categorie");
+
+            migrationBuilder.DropTable(name: "Commande");
+
+            migrationBuilder.DropTable(name: "Client");
+        }
+    }
+}
