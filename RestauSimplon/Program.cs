9652a505320ee4b6b554845a3ea5a2a52cdbd5e8
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-09 11:02:22 +0100

    ajout de la route get pour tout les articles

--- RestauSimplon/Program.cs
+++ RestauSimplon/Program.cs
@@ -13,13 +13,59 @@ builder.Services.AddDbContext<RestauDbContext>(options =>
     options.UseSqlite("Data Source=RestauSimplonDb.db");
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "RestauSimplon API",
+            Version = "v1",
+            Description = "Une API pour gérer le restaurant de Simplon",
+            Contact = new OpenApiContact
+            {
+                Name = "Simplon",
+                Email = "simplon@example.com",
+                Url = new Uri("https://simplon.co"),
+            },
+        }
+    );
+    c.EnableAnnotations();
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestauSimplon v1");
+        c.RoutePrefix = "";
+    });
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+// GET - Tout les articles
+app.MapGet(
+    "/articles",
+    [SwaggerOperation(
+        Summary = "Récupère la liste des articles",
+        Description = "Récupère la liste des articles du restaurant",
+        OperationId = "GetArticles",
+        Tags = new[] { "Articles" }
+    )]
+    async (RestauDbContext db) =>
+    {
+        return await db.Article.ToListAsync();
+    }
+);
 
 app.UseHttpsRedirection();
 
