fcfd74ded89ac3e71701568464987dacf2fc0f46
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-10 11:56:32 +0100

    ajout async sur article et exception categories

--- RestauSimplon/Routes/ArticlesEndpoints.cs
+++ RestauSimplon/Routes/ArticlesEndpoints.cs
@@ -63,7 +63,7 @@ namespace RestauSimplon.Routes
             Tags = new[] { "Articles" }
         )]
         [SwaggerResponse(200, "OK!", typeof(IEnumerable<ArticleItemDTO>))]
-        static Task<IResult> CreateArticle(ArticleItemDTO articleDTO, RestauDbContext db)
+        static async Task<IResult> CreateArticle(ArticleItemDTO articleDTO, RestauDbContext db)
         {
             var article = new Article
             {
@@ -72,17 +72,17 @@ namespace RestauSimplon.Routes
                 CategorieId = articleDTO.CategorieId,
             };
 
-            // Si un champ est vide ou le prix est à 0, j'envoie une bad request (400)
+            // Si les champ nom/categorieId sont vide ou le prix est à 0, j'envoie une bad request (400)
             if (string.IsNullOrEmpty(article.Nom) || article.Prix == 0 || article.CategorieId == 0)
             {
-                return Task.FromResult<IResult>(Results.BadRequest());
+                return Results.BadRequest(
+                    "Erreur, veuillez renseignez tout les champs correctement."
+                );
             }
 
             db.Article.Add(article);
-            db.SaveChanges();
-            return Task.FromResult<IResult>(
-                Results.Created($"/articles/{article.Id}", new ArticleItemDTO(article))
-            );
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/articles/{article.Id}", new ArticleItemDTO(article));
         }
 
         // PUT - Mettre a jour un article
